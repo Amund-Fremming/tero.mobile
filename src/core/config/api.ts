084fec4218eff28f1381f860f61132904e644558
amund-fremming <amund.fremming@gmail.com>
2026-03-04 18:40:25 +0100

    fix: Update API base URLs for local development and comment out ngrok URLs

--- src/core/config/api.ts
+++ src/core/config/api.ts
@@ -1,7 +1,7 @@
 /// Local setup
-//export const HUB_URL_BASE = "http://127.0.0.1:9000/hubs";
-//export const PLATFORM_URL_BASE = "http://127.0.0.1:3000";
+export const HUB_URL_BASE = "http://127.0.0.1:9000/hubs";
+export const PLATFORM_URL_BASE = "http://127.0.0.1:3000";
 
 /// Local port forarding setup
-export const HUB_URL_BASE = "https://5008-217-13-29-59.ngrok-free.app/hubs";
-export const PLATFORM_URL_BASE = "https://4cf7-217-13-29-59.ngrok-free.app";
+// export const HUB_URL_BASE = "https://5008-217-13-29-59.ngrok-free.app/hubs";
+// export const PLATFORM_URL_BASE = "https://4cf7-217-13-29-59.ngrok-free.app";
