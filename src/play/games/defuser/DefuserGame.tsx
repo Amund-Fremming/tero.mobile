ba7eeae8afb8f4965026849ec9ef0450ba47dff5
amund-fremming <amund.fremming@gmail.com>
2026-03-28 16:46:05 +0700

    fix: Correct import order and unify string quotes in DefuserGame component

--- src/play/games/defuser/DefuserGame.tsx
+++ src/play/games/defuser/DefuserGame.tsx
@@ -1,6 +1,6 @@
 import ScreenHeader from "@/src/core/components/ScreenHeader/ScreenHeader";
 import Color from "@/src/core/constants/Color";
-import { View, Text } from "react-native";
+import { Text, View } from "react-native";
 import { styles } from "./defuserGameStyles";
 
 export default function DefuserGame() {
@@ -8,7 +8,7 @@ export default function DefuserGame() {
     <View style={styles.container}>
       <ScreenHeader title="Defuser" />
       <View style={styles.content}>
-        <Text style={{ color: Color.Black, fontSize: 32, textAlign: 'center', marginTop: 40 }}>Defuser</Text>
+        <Text style={{ color: Color.Black, fontSize: 32, textAlign: "center", marginTop: 40 }}>Defuser</Text>
       </View>
     </View>
   );
